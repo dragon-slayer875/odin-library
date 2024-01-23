7d372ffbbd9b6058e548c3cdf8ba9e297be77156
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-01-23 22:17:11 +0530

    edit project to work as todolist

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const inputPage = document.querySelector('.input-page');
 const inputForm = document.querySelector('.input-form');
 const form = document.querySelector('form');
 const canvas = document.querySelector('.main-body');
-const card = document.querySelector('.book-card')
-const readButton = document.querySelector('.read-button')
+const card = document.querySelector('.done-card')
+const doneButton = document.querySelector('.done-button')
 const removeButton = document.querySelector('.remove-button');
 
 function addBooks() {
@@ -15,58 +15,45 @@ function addBooks() {
         const newCard = card.cloneNode(true);
         newCard.style.display = 'grid'
         newCard.querySelector('#title').textContent = `"${book.title}"`;
-        newCard.querySelector('#author').textContent = `${book.author}`;
-        newCard.querySelector('#pages').textContent = `${book.pages}`;
+        newCard.querySelector('#description').textContent = `${book.description}`;
+        newCard.querySelector('#time').textContent = `${book.time}`;
         newCard.dataset.id = book.id;
-        if (book.readStatus == "on") {
-            newCard.querySelector('.read-button').classList.add('green-button');
-            newCard.querySelector('.read-button').textContent = `Read`;
-        }
-        newCard.querySelector('.read-button').addEventListener('click', (e) => {
+        if (book.doneStatus == "on") {
+            newCard.querySelector('.done-button').classList.add('green-button');
+            newCard.querySelector('.done-button').textContent = `Done`;}
+        newCard.querySelector('.done-button').addEventListener('click', (e) => {
             e.target.classList.toggle('green-button');
-            toggleReadStatus(e.target.parentNode.dataset.id, e.target)
-        })
+            toggleDoneStatus(e.target.parentNode.dataset.id, e.target)})
         newCard.querySelector('.remove-button').addEventListener('click', (e) => {
             myLibrary.splice(e.target.parentNode.dataset.id, 1);
             addBooks()
             if (!myLibrary.length) {
-                canvas.innerHTML = `Nothing to show here. Add new books!`
-            }
-        })
-        canvas.appendChild(newCard);
-    })
-}
+                canvas.innerHTML = `Nothing to show here. Add new tasks!`}})
+        canvas.appendChild(newCard);})}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const formData = Object.fromEntries(new FormData(e.target).entries());
     myLibrary.push(formData);
     addBooks();
-    inputPage.classList.toggle('active');
-})
+    inputPage.classList.toggle('active');})
 
 newButton.addEventListener('click', () => {
-    inputPage.classList.toggle('active');
-})
+    inputPage.classList.toggle('active');})
 
 inputPage.addEventListener('click', () => {
-    inputPage.classList.toggle('active');
-})
+    inputPage.classList.toggle('active');})
 
 inputForm.addEventListener('click', (e) => {
-    e.stopPropagation();
-})
+    e.stopPropagation();})
 
-function toggleReadStatus(index, elem) {
-    if (myLibrary[index].readStatus == "on") {
-        myLibrary[index].readStatus = 'off';
-        elem.textContent = 'Not Read';
-        return;
-    }
-    myLibrary[index].readStatus = 'on';
-        elem.textContent = 'Read';
-}
+function toggleDoneStatus(index, elem) {
+    if (myLibrary[index].doneStatus == "on") {
+        myLibrary[index].doneStatus = 'off';
+        elem.textContent = 'Not Done';
+        return}
+    myLibrary[index].doneStatus = 'on';
+        elem.textContent = 'Done';}
 
 if (!myLibrary.length) {
-    canvas.innerHTML = `Nothing to show here. Add new books!`
-}
+    canvas.innerHTML = `Nothing to show here. Add new tasks!`}
